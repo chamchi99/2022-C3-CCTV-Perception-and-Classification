c1b675e705a0cb4ce3a803d2adfb165b510c0595
daewon88 <cdwer098@naver.com>
2022-12-15 16:46:49 +0900

    Update build_cdataset.py

--- osnet_training/build_cdataset.py
+++ osnet_training/build_cdataset.py
@@ -7,6 +7,7 @@ label_files = glob.glob('reid-data/cdataset/label/*.txt') # hard coded path
 os.makedirs('reid-data/cdataset/train') # hard coded path
 os.makedirs('reid-data/cdataset/test') # hard coded path
 
+# hard coded path
 with open('reid-data/cdataset/train_ids.txt', 'r') as tr:
     train_ids = tr.readline().split(',')[:-1]
 
