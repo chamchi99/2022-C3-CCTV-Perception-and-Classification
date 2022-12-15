81f2d90df0400ff1922e165c2c4caf9cd09d558e
daewon88 <cdwer098@naver.com>
2022-12-15 17:17:20 +0900

    Update preprocess.sh

--- osnet_training/preprocess.sh
+++ osnet_training/preprocess.sh
@@ -1,4 +1,4 @@
-python build_cdataset_split.py &&
-python build_cdataset.py &&
+python build_cdataset_split.py && # split train-test data 
+python build_cdataset.py && # crop image using label data
 python build_cdataset_query.py &&
 python idx_convert.py
